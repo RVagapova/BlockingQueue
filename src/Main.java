049f6f1415a7019a058a1a44b77464fdec5d3e98
Regina <regina.vagapova95@gmail.com>
2023-12-05 21:25:19 +0300

    added method

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ public class Main {
     private static BlockingQueue<String> queueOfText2 = new ArrayBlockingQueue<>(100);
     private static BlockingQueue<String> queueOfText3 = new ArrayBlockingQueue<>(100);
 
+    private static List<String> resultText = new CopyOnWriteArrayList<>();
+
 
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(4);
-        List<String> resultText = new CopyOnWriteArrayList<>();
+        CountDownLatch latch = new CountDownLatch(3);
 
         Runnable fillingQueue = () -> {
-            System.out.println("первый");
             for (int i = 0; i < 10_000; i++) {
                 try {
                     String text = generateText("abc", 100_000);
@@ -25,65 +26,11 @@ public class Main {
             }
         };
 
-        Runnable maxA = () -> {
-            String resultA = null;
-            int a = 0;
-            for (int i = 0; i < 10_000; i++) {
-                try {
-                    String tempString = queueOfText1.take();
-                    int tempA = countOfCharacter(tempString, 'a');
-                    if (a < tempA) {
-                        a = tempA;
-                        resultA = tempString;
-                    }
-                } catch (InterruptedException ignored) {
-                }
-            }
-            resultText.add(resultA);
-        };
-
-        Runnable maxB = () -> {
-            String resultB = null;
-            int b = 0;
-            for (int i = 0; i < 10_000; i++) {
-                try {
-                    String tempString = queueOfText2.take();
-                    int tempB = countOfCharacter(tempString, 'b');
-                    if (b < tempB) {
-                        b = tempB;
-                        resultB = tempString;
-                    }
-                } catch (InterruptedException e) {
-                    return;
-                }
-            }
-            resultText.add(resultB);
-        };
-
-        Runnable maxC = () -> {
-            String resultC = null;
-            int c = 0;
-            for (int i = 0; i < 10_000; i++) {
-                try {
-                    String tempString = queueOfText3.take();
-                    int tempC = countOfCharacter(tempString, 'c');
-                    if (c < tempC) {
-                        c = tempC;
-                        resultC = tempString;
-                    }
-                } catch (InterruptedException e) {
-                    return;
-                }
-            }
-            resultText.add(resultC);
-        };
-
         executor.submit(fillingQueue);
-        executor.submit(maxA);
-        executor.submit(maxB);
-        executor.submit(maxC);
-
-        executor.awaitTermination(1, TimeUnit.MINUTES);
+        executor.submit(searchLineWithMaxCharacter(queueOfText1, 'a', latch));
+        executor.submit(searchLineWithMaxCharacter(queueOfText2, 'b', latch));
+        executor.submit(searchLineWithMaxCharacter(queueOfText3, 'c', latch));
+        latch.await();
         executor.shutdown();
 
         resultText.forEach(System.out::println);
@@ -96,6 +43,26 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+
+    public static Runnable searchLineWithMaxCharacter(BlockingQueue<String> str, char character, CountDownLatch latch) {
+        return () -> {
+            String result = null;
+            int countOfCharacter = 0;
+            for (int i = 0; i < 10_000; i++) {
+                try {
+                    String tempString = str.take();
+                    int tempC = countOfCharacter(tempString, character);
+                    if (countOfCharacter < tempC) {
+                        countOfCharacter = tempC;
+                        result = tempString;
+                    }
+                } catch (InterruptedException ignored) {
+                }
+            }
+            resultText.add(result);
+            latch.countDown();
+        };
     }
 
     public static int countOfCharacter(String text, char symbol) {
